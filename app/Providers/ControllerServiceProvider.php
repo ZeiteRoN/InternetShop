e59564d18434ec27ee20e4d43bb788f6b2aea683
ZeiteRoN <zeiter1007@gmail.com>
2024-05-07 20:20:41 +0300

    Init Commit

---
+++ app/Providers/ControllerServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ControllerServiceProvider extends ServiceProvider
+{
+    /*** Bootstrap services.*/
+    public function boot(): void
+    {
+        //
+    }
+}
